1375e7b2bcece5bf1c90ebc2af693fc25a9ae9ed
Zane Kozmak <zkozmak1@nait.ca>
2022-01-25 18:06:52 -0700

    Jan 18

--- dmit2015-1212-oa01-demos-zkozmak1/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
+++ dmit2015-1212-oa01-demos-zkozmak1/src/test/java/ca/nait/dmit/domain/AlbertaCovid19CaseManagerTest.java
@@ -1,17 +1,45 @@
 package ca.nait.dmit.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlbertaCovid19CaseManagerTest {
 
+    AlbertaCovid19CaseManager caseManager;
+    @BeforeEach
+    void beforeEach() throws IOException {
+        caseManager = AlbertaCovid19CaseManager.getInstance();
+    }
+
     @Test
-    void getAlbertaCovid19CaseList() throws IOException {
-        //AlbertaCovid19CaseManager caseManager = new AlbertaCovid19CaseManager();
-        AlbertaCovid19CaseManager caseManager = AlbertaCovid19CaseManager.getInstance();
+    void getAlbertaCovid19CaseList()
+    {
         assertEquals(466616, caseManager.getAlbertaCovid19CaseList().size());
+    }
+
+    @Test
+    void activeCases()
+    {
+        assertEquals(61_615, caseManager.countTotalActiveCases());
+    }
+
+    @Test
+    void activeCasesByZone()
+    {
+        assertEquals(29_867, caseManager.countActiveCasesByAhsZone("Calgary Zone"));
+        assertEquals(21_317, caseManager.countActiveCasesByAhsZone("Edmonton Zone"));
+    }
+
+    @Test
+    void distinctAhsZones()
+    {
+        List<String> ahsZoneList = caseManager.distinctAhsZones();
+        ahsZoneList.forEach(System.out::println);
+        assertEquals(6, ahsZoneList.size());
     }
 }
